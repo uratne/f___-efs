02eb88b2493bf3ae8124f1cd73cdc5dbf5a1b1c3
Uditha Gunaratne <gunaratne1996@gmail.com>
2024-11-10 17:30:50 +0530

    proper ws closing and managing with ping/pong

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,13 +1,21 @@
+use std::env;
+
 use futures_util::{SinkExt, StreamExt};
+use log::{error, info};
 use tungstenite::handshake::client::{generate_key, Request};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
 #[tokio::main]
 async fn main() {
-    let host = "localhost:8080";
-    let path = "/ws";
-    let uri = format!("ws://{}{}", host, path);
+    dotenv::dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let host = env::var("HOST").unwrap_or_else(|_| "localhost".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let path = "ws";
+    let host = format!("{}:{}", host, port);
+    let uri = format!("ws://{}/{}", host, path);
 
+    info!("connecting to {}", uri);
     // Connect to WebSocket server
     let mut request = Request::builder()
         .uri(uri)
@@ -21,7 +29,7 @@ async fn main() {
 
     request.headers_mut().insert("api-key", "42".parse().unwrap());
     let (ws_stream, _) = connect_async(request).await.expect("Failed to connect");
-    println!("WebSocket connected");
+    info!("webSocket connected");
 
     // Split the WebSocket stream
     let (mut write, mut read) = ws_stream.split();
@@ -32,12 +40,12 @@ async fn main() {
             match message {
                 Ok(msg) => {
                     match msg {
-                        Message::Text(text) => println!("Received: {}", text),
-                        Message::Binary(data) => println!("Received binary data: {:?}", data),
-                        Message::Ping(_) => println!("Received ping"),
-                        Message::Pong(_) => println!("Received pong"),
+                        Message::Text(text) => info!("Received: {}", text),
+                        Message::Binary(data) => info!("Received binary data: {:?}", data),
+                        Message::Ping(_) => info!("Received ping"),
+                        Message::Pong(_) => info!("Received pong"),
                         Message::Close(_) => {
-                            println!("Server closed connection");
+                            info!("Server closed connection");
                             break;
                         },
                         Message::Frame(_) => println!("Received raw frame"),
@@ -57,14 +65,14 @@ async fn main() {
         let message = lib::message::Message::new("Hello from client!".to_string(), "test client".to_string());
         let message = serde_json::to_string(&message).unwrap();
         if let Err(e) = write.send(Message::Text(message.clone())).await {
-            eprintln!("Error sending message: {}", e);
+            error!("Error sending message: {}", e);
         }
 
         // Keep the connection alive
         loop {
             tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
             if let Err(e) = write.send(Message::Ping(vec![])).await {
-                eprintln!("Error sending ping: {}", e);
+                error!("Error sending ping: {}", e);
                 break;
             }
         }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,6 +1,9 @@
+use std::time::Duration;
+
 use actix_web::{rt, web, Error, HttpRequest, HttpResponse, Result};
-use actix_ws::AggregatedMessage;
+use actix_ws::{AggregatedMessage, ProtocolError, Session};
 use log::{error, info};
+use tokio::time::sleep;
 
 use crate::message::Message;
 
@@ -8,57 +11,79 @@ use crate::message::Message;
 pub async fn listen(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
     info!("WebSocket connection request from {}", req.peer_addr().unwrap());
     let (res, mut session, stream) = actix_ws::handle(&req, stream)?;
-
+    
     let mut stream = stream
-        .aggregate_continuations()
-        // aggregate continuation frames up to 1MiB
-        .max_continuation_size(2_usize.pow(20));
-
+    .aggregate_continuations()
+    // aggregate continuation frames up to 1MiB
+    .max_continuation_size(2_usize.pow(20));
+    
     info!("WebSocket connection established");
-    // start task but don't wait for it
-    rt::spawn(async move {
-        // receive messages from websocket
-        while let Some(msg) = stream.recv().await{
-            match msg {
-                Ok(AggregatedMessage::Text(text)) => {
-                    // echo text message
-                    let message: Result<Message, serde_json::Error> = serde_json::from_str(&text);
-                    match message {
-                        Ok(message) => {
-                            info!("Received message: {:#?}", message);
-                        }
-                        Err(e) => {
-                            error!("Failed to parse message: {:?}", e);
-                        }
-                    }
-                }
 
-                Ok(AggregatedMessage::Binary(bin)) => {
-                    // echo binary message
-                    session.binary(bin).await.unwrap();
-                }
-
-                Ok(AggregatedMessage::Ping(msg)) => {
-                    // respond to PING frame with PONG frame
-                    session.pong(&msg).await.unwrap();
-                }
-
-                Ok(AggregatedMessage::Close(reason)) => {
-                    // close the session
-                    if reason.is_some() {
-                        info!("Closing session with reason code: {:?} and description: {:?}", reason.clone().unwrap().code, reason.clone().unwrap().description);
-                    } else {
-                        error!("Closing session without reason");
-                    }
-   
-                    session.close(reason).await.unwrap();
-                    break;
-                }
-
+    let mut ping_session = session.clone();
+    let handle = rt::spawn(async move {
+        while let Some(msg) = stream.recv().await {
+            match handle_message(msg, &mut session).await {
+                false => break,
                 _ => {}
             }
+            
         }
+        info!("webSocket connection closed");
     });
 
+    rt::spawn(async move {
+        let ping_interval = Duration::from_secs(10);
+        
+        while let Ok(()) = ping_session.ping(b"ping").await {
+            sleep(ping_interval).await;
+        }
+        
+        info!("Ping failed, aborting message handler");
+        handle.abort();
+        info!("WebSocket connection terminated by ping monitor");
+    });
+    
     Ok(res)
+}
+
+async fn handle_message(msg: Result<AggregatedMessage, ProtocolError>, session: &mut Session) -> bool {
+    match msg {
+        Ok(AggregatedMessage::Text(text)) => {
+            // echo text message
+            let message: Result<Message, serde_json::Error> = serde_json::from_str(&text);
+            match message {
+                Ok(message) => {
+                    info!("Received message: {:#?}", message);
+                }
+                Err(e) => {
+                    error!("Failed to parse message: {:?}", e);
+                }
+            }
+        }
+        
+        Ok(AggregatedMessage::Binary(bin)) => {
+            // echo binary message
+            session.binary(bin).await.unwrap();
+        }
+        
+        Ok(AggregatedMessage::Ping(msg)) => {
+            // respond to PING frame with PONG frame
+            session.pong(&msg).await.unwrap();
+        }
+        
+        Ok(AggregatedMessage::Close(reason)) => {
+            // close the session
+            if reason.is_some() {
+                info!("Closing session with reason code: {:?} and description: {:?}", reason.clone().unwrap().code, reason.unwrap().description);
+            } else {
+                error!("Closing session without reason");
+            }
+
+            return false;
+        }
+
+        _ => {}
+    }
+
+    true
 }
