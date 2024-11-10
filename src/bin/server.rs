77e07e5f0d373af008821ed09f04598ad358134d
Uditha Gunaratne <gunaratne1996@gmail.com>
2024-11-10 10:11:56 +0530

    Server & UI Init with Actix. Logger init with Env Logger

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,21 @@
-#[tokio::main]
-async fn main() {
-    lib::hello_world("server");
+use actix_web::{middleware, App, HttpServer};
+use actix_files as fs;
+use log::info;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    info!("Starting server at http://127.0.0.1:8080");
+    
+    HttpServer::new(|| {
+        App::new()
+            // Enable logger middleware
+            .wrap(middleware::Logger::default())
+            // Serve static files from the "static" directory
+            .service(fs::Files::new("/", "./static").index_file("index.html"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
